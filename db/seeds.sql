b83588e82754fe46008291b2ae2477122ace1feb
Karen V <karen.pretzel.365@gmail.com>
2021-09-14 16:56:38 -0500

    first changes

---
+++ db/seeds.sql
@@ -0,0 +1,35 @@
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Marketing"),
+       ("Accounting and Finance"),
+       ("Human Resource Management"),
+       ("Purchasing"),
+       ("Research and Development"),
+       ("Production"),
+       ("Customer Service"),
+       ("Delete this"),
+       ("Legal"),
+       ("Engineering");
+       
+INSERT INTO role (title,salary)
+VALUES ("Sales Lead",3000,"Sales"),
+       ("Lead Engineer",2000,"Enineering"),
+       ("Salesperson",1400,"Sales"),
+       ("Accountant",4500,"Accounting and Finance"),
+       ("Lawyer",3300,"Legal"),
+       ("Production Manager",1400,"Production"),
+       ("Software Engineer",37300,"Engineering"),
+       ("Account Manager",1600,"Accounting and Finance");
+      
+
+
+INSERT INTO role (first_name,last_name)
+VALUES ("John","Doe"),
+       ("Peter","Brown"),      
+       ("Sara","Johnson"),      
+       ("Tom","Chan"),      
+       ("Maria","Sanchez"),      
+       ("Sandra","Peterson"),      
+       ("Mike","Beckwith"),      
+       ("Pamela","Wessiman");      
+            
