6002bef810613ec730360efd846a9a6ce953509b
Karen V <karen.pretzel.365@gmail.com>
2021-09-14 21:39:50 -0500

    worked on database schema and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,25 +11,27 @@ VALUES ("Sales"),
        ("Legal"),
        ("Engineering");
        
-INSERT INTO role (title,salary)
-VALUES ("Sales Lead",3000,"Sales"),
-       ("Lead Engineer",2000,"Enineering"),
-       ("Salesperson",1400,"Sales"),
-       ("Accountant",4500,"Accounting and Finance"),
-       ("Lawyer",3300,"Legal"),
-       ("Production Manager",1400,"Production"),
-       ("Software Engineer",37300,"Engineering"),
-       ("Account Manager",1600,"Accounting and Finance");
+INSERT INTO role (title,salary,department_id)
+VALUES ("Sales Lead",3000,1),
+       ("Lead Engineer",2000,11),
+       ("Salesperson",1400,1),
+       ("Accountant",4500,3),
+       ("Lawyer",3300,10),
+       ("Production Manager",1400,7),
+       ("Software Engineer",37300,11),
+       ("Account Manager",1600,3);
       
 
 
-INSERT INTO role (first_name,last_name)
-VALUES ("John","Doe"),
-       ("Peter","Brown"),      
-       ("Sara","Johnson"),      
-       ("Tom","Chan"),      
-       ("Maria","Sanchez"),      
-       ("Sandra","Peterson"),      
-       ("Mike","Beckwith"),      
-       ("Pamela","Wessiman");      
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES ("John","Doe",1,NULL),
+       ("Peter","Brown",2,NULL),      
+       ("Sara","Johnson",7,2),      
+       ("Tom","Chan",3, 1),      
+       ("Maria","Sanchez",6, NULL), 
+       ("Sandra","Peterson",7,2),      
+       ("Mike","Beckwith",5,NULL),      
+       ("Pamela","Wessiman",4,NULL);      
+     
+      
             
