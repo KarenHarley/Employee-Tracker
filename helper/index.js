27f7da6650c2991d13e371515694d1253f1b7607
Karen V <karen.pretzel.365@gmail.com>
2021-09-17 16:23:09 -0500

    worked on adding Employee

--- helper/index.js
+++ helper/index.js
@@ -1,28 +1,66 @@
-/*
-//template for the classes
-class myClass {
-  
-    constructor() {
-      //stuff you need here
-    }
-  
-    
-    searchAllEmployees(someParam) {
-     //query you need here   }
-  }
-  */
 
-
-//const { createPromptModule } = require("inquirer");
+let roleId;
+let managerId;
 const db = require("../db/connection");
+//const { employee } = require("../helper");
 
+class Employee {
+  constructor(firstName, lastName, role, manager) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.role = role;
+    this.manager = manager;
+  }
+  getRole() {
+
+    return db
+      .promise()
+      .query("SELECT id,title FROM role")
+      .then(([results]) => {
+        //console.log(results);
+        let output = results.filter((role) => role.title == this.role);
+        //console.log(output);
+        roleId = output[0].id
+        //console.log(roleId)
+        this.getManager()
+      });
+  }
+  getManager() {
+    if (this.manager === "None"){
+      this.manager = null
+    }
+    return db
+      .promise()
+      .query("SELECT id,CONCAT(first_name, ' ', last_name) AS complete_name FROM `employee`")//CONCAT(first_name, ',', last_name) AS complete_name FROM `employee`;
+      .then(([results]) => {
+       // console.log(results);
+        let output = results.filter((people) => people.complete_name == this.manager);
+       // console.log(output);
+        managerId = output[0].id
+       // console.log(managerId)
+        this.addEmployeeToDb()
+      });
+  }
+  addEmployeeToDb() {
+
+    const query =
+      "INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES (?)";
+    let values = [this.firstName, this.lastName, roleId, managerId]; //"this.first_name,this.lastName"; //(?)
+    return db
+      .promise()
+      .query(query, [values])
+      .then(([results]) => {
+       // console.log("worked");
+      });
+  }
+}
 
 const getNames = () => {
   return db
     .promise()
     .query("SELECT first_name,last_name FROM employee")
     .then(([results]) => {
-      let employeeList = [];
+      let employeeList = ["None"];
       results.map((employee) => {
         employeeList.push([employee.first_name, employee.last_name].join(" "));
       });
@@ -37,15 +75,16 @@ const getRole = () => {
     .then(([results]) => {
       let roleList = [];
       results.map((role) => {
-        roleList.push([role.title]);
+        roleList.push([role.title].join(" "));
       });
       return roleList;
     });
 };
-
+//employee = new Employee("Jack", "Ryan", "Software Engineer", "None").getRole(); //.addEmployeeToDb()
+//employee2 = new Employee("Daniel", "Spencer", "Account Manager", "Karen Villagomez").getRole(); //.addEmployeeToDb()
 //getNames();
 //getRole();
-getNames().then((response) => console.log(response));
-console.log(getRole());
-
-module.exports = { getNames };
+//getNames().then((response) => console.log(response));
+//getRole().then((response) => console.log(response));
+//console.log(roleId);
+module.exports = { getNames, getRole, Employee };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var inquirer = require("inquirer");
-const { getNames } = require("./helper");
-const cTable = require("console.table")
+const { getNames, getRole, Employee} = require("./helper");
+const cTable = require("console.table");
+let employee;
 /*
 Things to do:
 1. Make ALL functions in ES6 (arrow)
@@ -48,24 +49,21 @@ function appMenu() {
           addDepartment();
           break;
         default:
-          //Quit somehow
-         
+        //Quit somehow
       }
     });
 
-  const viewAllEmployees =() => {
+  const viewAllEmployees = () => {
     /*
       THEN I am presented
        with a formatted table showing 
        department names and department ids
       */
-  }
+  };
   const addEmployee = async () => {
-    const employeeInfo = await getNames
+    const employeeInfo = await getNames;
+    const roleInfo = await getRole;
 
-  //  if(!employeeInfo.ok){
-  //    throw new Error("something is wrong")
-  //  }
     inquirer
       .prompt([
         {
@@ -94,40 +92,29 @@ function appMenu() {
           type: "list",
           name: "role",
           message: "What is your employee's role?",
-          choices: [
-            "Sales",
-            "Marketing",
-            "Accounting and Finance",
-            "Human Resource Management",
-            "Purchasing",
-            "Research and Development",
-            "Production",
-            "Customer Service",
-            "Delete this",
-            "Legal",
-            "Engineering",
-          ],
+          choices: roleInfo,
         },
         {
           type: "list",
           name: "manager",
           message: "What is your employee's manager?",
-          choices: employeeInfo,//to something like push("None")
+          choices: employeeInfo,
         },
       ])
       .then((answers) => {
-        //const employee = new Employee(
-        //  answers.engineerName
-        //create things for the object (properties)
-        //);
-
+        employee = new Employee(
+          answers.firstName,
+          answers.lastName,
+          answers.role,
+          answers.manager
+        );
+          employee.getRole()//calling the first function
         console.log(
           `Added ${answers.firstName} ${answers.lastName} to the database!`
         );
         appMenu(); //call the first questions
       });
-      
-  }
+  };
   function updateEmployeeRole() {
     inquirer
       .prompt([
@@ -175,11 +162,11 @@ function appMenu() {
     */
   }
   function addDepartment() {}
-  
 }
 
 appMenu();
 
+module.exports = { employee };
 /*
   .then((answers) => {
     // Use user feedback for... whatever!!
